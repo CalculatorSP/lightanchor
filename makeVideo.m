7ac38ff3d44463a3e007cdb47f7b6a2f9f33410a
CalculatorSP <mike.handidate@gmail.com>
2018-06-27 19:29:24 -0400

    Simplify panorama projection function and add rotation matrix input

--- makeVideo.m
+++ makeVideo.m
@@ -2,37 +2,33 @@
 FOV = 60 * pi / 180;
 FW = 1280;
 FH = 720;
-
-V = 2 * tan(FOV / 2) / hypot(FW, FH);
-
 [px, py] = meshgrid(1:FW, 1:FH);
-x = px - FW / 2;
-y = -(py - FH / 2);
-d = hypot(x, y);
-phi = atan2(y, x);              % -pi to pi (left to left, ccw looking down)
-theta = atan(V * d) - pi / 2;   % -pi / 2 to 0 (south pole to equator)
-r = ones(size(px));
+p = [px(:) - FW / 2, -(py(:) - FH / 2), -hypot(FW, FH) / 2 * cot(FOV / 2) * ones(FW * FH, 1)];
 
 % PANORAMA-SPECIFIC SETUP
 IMFILE = 'morning.png';
 I = imread(IMFILE);
-figure;
-imshow(I);
-
 [H, W, C] = size(I);
-sphi = round(W * (pi - phi) / (2 * pi));            % 0 to W (left to left, cw looking down)
-stheta = round(H * (pi - 2 * theta) / (2 * pi));    % H / 2 to H (equator to south pole)
-sphi(sphi < 1) = 1;
-sphi(sphi > W) = W;
-stheta(stheta < 1) = 1;
-stheta(stheta > H) = H;
 
-indices = H * (sphi - 1) + stheta;
+% Apply rotation matrix to p
+R = [1 0 0; 0 cosd(90) -sind(90); 0 sind(90) cosd(90)];
+rp = p * R.';
+
+% Generate panorama coordinates
+stheta = round(W * (pi - atan2(rp(:,2), rp(:,1))) / (2 * pi));                      % 0 to W (left to left, cw looking down)
+sphi = round(H * acos(rp(:,3) ./ sqrt(rp(:,1).^2 + rp(:,2).^2 + rp(:,3).^2)) / pi); % 0 to H (north pole to south pole)
+stheta(stheta < 1) = 1;
+stheta(stheta > W) = W;
+sphi(sphi < 1) = 1;
+sphi(sphi > H) = H;
+
+% Build the frame by reading panorama memory
+indices = H * (stheta - 1) + sphi;
 R = I(indices);
 G = I(H * W + indices);
 B = I(2 * H * W + indices);
 J = reshape([R, G, B], FH, FW, C);
 
-figure;
-imshow(J);
+figure; imshow(I);
+figure; imshow(J);
 
