4144eb9489222780c35f7c4655bf0a086d705923
John Miller <jmiller4@andrew.cmu.edu>
2018-08-07 16:40:06 -0400

    Add pre-processing and clean up detection script

--- detectLED.m
+++ detectLED.m
@@ -1,10 +1,13 @@
 function [loc] = detectLED(vid)
 
-h = zeros(1, 1, 1, size(vid, 4));
+[H, ~, ~, T] = size(vid);
+
+g_vid = double(vid(:, :, 1, :)) / 255;
+h = zeros(1, 1, 1, T);
 h(1, 1, 1, 1:2:end) = 255;
-res = convn(vid, h(1, 1, 1, end:-1:1), 'valid');
+res = convn(g_vid, h(1, 1, 1, end:-1:1), 'valid');
 [~, idx] = max(res(:));
-loc = [ceil(idx/size(vid, 1)), mod(idx, size(vid, 1))];
+loc = [ceil(idx/H), mod(idx, H)];
 
 end
 
