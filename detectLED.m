0a922c928997da5071dc0845ee088b742cb4b549
John Miller <jmiller4@andrew.cmu.edu>
2018-08-07 23:57:14 -0700

    Fix scale of h

--- detectLED.m
+++ detectLED.m
@@ -4,7 +4,7 @@ function [loc] = detectLED(vid)
 
 g_vid = double(vid(:, :, 1, :)) / 255;
 h = zeros(1, 1, 1, T);
-h(1, 1, 1, 1:2:end) = 255;
+h(1, 1, 1, 1:2:end) = 1;
 res = convn(g_vid, h(1, 1, 1, end:-1:1), 'valid');
 [~, idx] = max(res(:));
 loc = [ceil(idx/H), mod(idx, H)];
